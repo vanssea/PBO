b9ba733396bcd0b6c2451fea746328937b628a07
vanssea <cristinvanessa6@gmail.com>
2024-12-01 23:29:51 +0700

    Kuis 2 Perpustakaan

---
+++ Perpustakaan/src/backend/Kategori.java
@@ -0,0 +1,142 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.ArrayList;
+import java.sql.*;
+
+
+public class Kategori {
+    private int idkategori;
+    private String nama;
+    private String keterangan;
+    
+    public int getIdkategori() {
+        return idkategori;
+    }
+
+    public void setIdkategori(int idkategori) {
+        this.idkategori = idkategori;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    @Override
+    public String toString() {
+        return nama; // Tampilkan nama kategori di JComboBox
+    }
+    
+    public String getKeterangan() {
+        return keterangan;
+    }
+    
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+    
+    public Kategori()
+    {
+        
+    } 
+    
+    public Kategori(String nama, String keterangan)
+    {
+        this.nama = nama;
+        this.keterangan = keterangan;
+    } 
+    
+    public Kategori getById(int id)
+    {
+        Kategori kat = new Kategori();
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM kategori "
+                                             + "WHERE idkategori = '" + id + "'");
+        
+        try
+        {
+            while(rs.next())
+            {
+                kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama (rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
+            }    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return kat;
+    }    
+
+    public ArrayList<Kategori> getAll() {
+        ArrayList<Kategori> ListKategori = new ArrayList<> ();
+        
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM kategori");
+        
+        try {
+            while (rs.next()) {
+                Kategori kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama (rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
+                
+                ListKategori.add(kat);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ListKategori;
+    }
+    
+public ArrayList<Kategori> search(String keyword){
+        ArrayList<Kategori> ListKategori = new ArrayList<>();
+
+        String sql = "SELECT * FROM kategori WHERE "
+                   + " nama LIKE '%" + keyword + "%' "
+                   + " OR keterangan LIKE '%" + keyword + "%'";
+
+        ResultSet rs = DBHelper.selectQuery(sql);
+
+        try{
+            while(rs.next()){
+                Kategori kat = new Kategori();
+                kat.setIdkategori(rs.getInt("idkategori"));
+                kat.setNama(rs.getString("nama"));
+                kat.setKeterangan(rs.getString("keterangan"));
+
+                ListKategori.add(kat);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return ListKategori;
+    }
+    
+    public void save(){
+        if(getById(idkategori).getIdkategori()== 0) {
+            String SQL = "INSERT INTO kategori (nama, keterangan) VALUES ( "
+                        + "'" + this.nama + "', "
+                        + "'" + this.keterangan + "'"
+                        + ")";
+            this.idkategori = DBHelper.insertQueryGetId(SQL);
+        }
+    }
+    
+    public void delete() {
+        String SQL = "DELETE FROM kategori WHERE idkategori = '" + this.idkategori + "'";
+        DBHelper.executeQuery(SQL);
+    }
+}
---
+++ Perpustakaan/src/backend/TestBackend.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TestBackend {
+    public static void main(String[] args) {
+        Kategori kat1 = new Kategori ("Novel", "Koleksi buku novel");
+        Kategori kat2 = new Kategori ("Referensi", "Buku referensi ilmiah");
+        Kategori kat3 = new Kategori ("Komik", "Komik anak-anak");
+        
+        //test insert
+        kat1.save();
+        kat2.save();
+        kat3.save();
+        
+        //test update
+        kat2.setKeterangan ("Koleksi buku referensi ilmiah");
+        kat2.save();
+        
+        //test delete
+        kat3.delete();
+        
+        //test select all
+        for (Kategori k : new Kategori().getAll()) {
+            System.out.println("Nama: "+ k.getNama() + ", Ket: " + k.getKeterangan());
+            
+        }
+    }
+    
+}
+
